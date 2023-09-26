831185cc6a6d4235792c73a4776347036495eedb
VishLella <lella@purdue.edu>
2023-09-26 00:51:11 -0400

    Initial commit

---
+++ src/main/java/com/lella/coronavirusupdates/CoronavirusUpdatesApplication.java
@@ -0,0 +1,13 @@
+package com.lella.coronavirusupdates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusUpdatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusUpdatesApplication.class, args);
+	}
+
+}
